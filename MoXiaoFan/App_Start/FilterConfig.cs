3bd1d9f718675331b1e653426eff4e9d9061505c
ljc38179 <ljc38179@ly.com>
2016-12-23 10:53:04 +0800

    For the first time(Template)

---
+++ MoXiaoFan/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using MoXiaoFan.Infrastructure;
+
+namespace MoXiaoFan.App_Start
+{
+    public class FilterConfig
+    {
+        public static void Configure(System.Web.Mvc.GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
